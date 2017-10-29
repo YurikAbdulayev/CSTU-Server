f3b4e9d49f1076d326ed6d896b00daccb1c2947e
YuriiA <yurs11k89@gmail.com>
2017-10-29 08:30:38 +0200

    create job for getting schedule

---
+++ src/main/java/com/cstu/json/objects/ScheduleInfo.java
@@ -0,0 +1,20 @@
+package com.cstu.json.objects;
+
+public class ScheduleInfo {
+
+    String lessonType;
+    String lessonName;
+    String teacher;
+    String lessonRoom;
+    String lessonDetails;
+    String fullData;
+
+    public ScheduleInfo(String lessonType, String lessonName, String teacher, String lessonRoom, String lessonDetails, String fullData) {
+        this.lessonType = lessonType;
+        this.lessonName = lessonName;
+        this.teacher = teacher;
+        this.lessonRoom = lessonRoom;
+        this.lessonDetails = lessonDetails;
+        this.fullData = fullData;
+    }
+}
---
+++ src/main/java/com/cstu/web/ParserRestController.java
@@ -0,0 +1,29 @@
+package com.cstu.web;
+
+import com.cstu.service.ParserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
+
+@RestController
+@RequestMapping("/api")
+public class ParserRestController {
+
+    private final ParserService parserService;
+
+    @Inject
+    public ParserRestController(ParserService parserService) {
+        this.parserService = parserService;
+    }
+
+    @GetMapping("/parser/{group}/{sdate}/{edate}")
+    public ResponseEntity<?> getSchedule (@PathVariable String group,
+                                                          @PathVariable String sdate,
+                                                          @PathVariable String edate) {
+        return ResponseEntity.ok().body(parserService.getSchedule(group, sdate, edate));
+    }
+}
